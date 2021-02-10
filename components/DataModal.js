eaed4fbc0d3e710c6b1013e861dc7e7387d1f070
Prasad <prasad@laptop123>
2021-02-11 03:02:45 +0530

    modified DataModal file

--- components/DataModal.js
+++ components/DataModal.js
@@ -6,32 +6,32 @@ function DataModal({ modalData, modalIsOpen, setModalIsOpen }) {
   return (
     <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
       <div className="flex items-center justify-center h-full">
-        <div className="">
-          <div className="px-24">
+        <div className="relative px-6 py-4 border-2">
+          <div className="px-32">
             <img
               className="w-36 h-36"
               src={modalData && modalData.image}
               alt={modalData && modalData.image.slice(1)}
             />
           </div>
-          <div className="mt-5 mb-2 text-lg">
-            <span className="text-xl font-semibold">Name</span> -
+          <div className="mt-5 mb-2 text-xl font-semibold">
+            <span className="text-lg font-medium ">Name -</span>{" "}
             {modalData.category}
           </div>
-          <div className="my-2 text-lg">
-            <span className="text-xl font-semibold">Description</span> -{" "}
+          <div className="my-2 text-xl font-semibold">
+            <span className="text-lg font-medium ">Description -</span>{" "}
             {modalData.description}
           </div>
-          <div className="my-2 text-lg">
-            <span className="text-xl font-semibold">Quantity</span> -
+          <div className="my-2 text-xl font-semibold">
+            <span className="text-lg font-medium ">Quantity -</span>{" "}
             {modalData.quantity}
           </div>
-        </div>
-        <div
-          onClick={() => setModalIsOpen(false)}
-          className="p-2 mb-56 rounded-full hover:shadow-xl hover:bg-gray-500 active:text-red-600"
-        >
-          <GrClose size="1.5rem" />
+          <div
+            onClick={() => setModalIsOpen(false)}
+            className="absolute p-2 bg-gray-300 rounded-full -right-5 -top-5 hover:shadow-xl hover:bg-gray-400 active:text-red-600"
+          >
+            <GrClose size="1.5rem" />
+          </div>
         </div>
       </div>
     </Modal>
